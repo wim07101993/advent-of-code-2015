1b47781a395bf6cb8cae571ab799b742faa60e06
wim van laer <van.laer.wim@gmail.com>
2019-09-02 10:13:43 +0200

    split parsing of input in seperate funcs

--- day02/day02.go
+++ day02/day02.go
@@ -1,7 +1,6 @@
 package day02
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 )
@@ -18,14 +17,20 @@ type WrappingPaperDimensions struct {
 	Width  int
 }
 
-func New(inputProvider func() []string) *SolverDay2 {
+func New(inputProvider func() []string) (*SolverDay2, error) {
 	strInput := inputProvider()
-	return &SolverDay2{
-		input: ParseInput(strInput),
+	wpds, err := ParseInput(strInput)
+	if err != nil {
+		return nil, err
 	}
+
+	return &SolverDay2{
+		input: wpds,
+	}, nil
 }
 
 func (s *SolverDay2) SolvePart1() string {
+
 	return ""
 }
 
@@ -33,34 +38,38 @@ func (s *SolverDay2) SolvePart2() string {
 	return ""
 }
 
-func ParseInput(input []string) []WrappingPaperDimensions {
+func ParseInput(input []string) ([]WrappingPaperDimensions, error) {
 	ret := []WrappingPaperDimensions{}
+	for _, s := range input {
+		wpd, err := ParseWrappingPaperDimensions(s)
+		if err != nil {
+			return nil, err
+		}
+		ret = append(ret, wpd)
+	}
+	return ret, nil
+}
 
+func ParseWrappingPaperDimensions(input string) (WrappingPaperDimensions, error) {
 	l := 0
 	w := 0
 	h := 0
 	var err error
-	for _, s := range input {
-		split := strings.Split(s, "x")
-		if l, err = strconv.Atoi(split[0]); err != nil {
-			fmt.Println("Error while parsing input:", err)
-			continue
-		}
-		if w, err = strconv.Atoi(split[1]); err != nil {
-			fmt.Println("Error while parsing input:", err)
-			continue
-		}
-		if h, err = strconv.Atoi(split[2]); err != nil {
-			fmt.Println("Error while parsing input:", err)
-			continue
-		}
 
-		ret = append(ret, WrappingPaperDimensions{
-			Length: l,
-			Width:  w,
-			Height: h,
-		})
+	split := strings.Split(input, "x")
+	if l, err = strconv.Atoi(split[0]); err != nil {
+		return WrappingPaperDimensions{}, err
+	}
+	if w, err = strconv.Atoi(split[1]); err != nil {
+		return WrappingPaperDimensions{}, err
+	}
+	if h, err = strconv.Atoi(split[2]); err != nil {
+		return WrappingPaperDimensions{}, err
 	}
 
-	return ret
+	return WrappingPaperDimensions{
+		Length: l,
+		Width:  w,
+		Height: h,
+	}, nil
 }
